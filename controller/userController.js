4945aa51eb8617bb18bfa3916c138c2bd47f60dd
olivier Rukundo <rr.olivier01@gmail.com>
2025-05-20 12:18:05 +0200

    usercontrolle.js modified to send request to postman

---
+++ controller/userController.js
@@ -0,0 +1,16 @@
+
+const userModel = require('../model/usermodel')
+
+
+exports.createUser = async (req, res) => {
+
+    const { fullName,email,password } = req.body
+try {
+    const user = await userModel.create({fullName,email,password})
+    res.json({message: 'User created successfully', user})
+} catch (error) {
+    console.log('error occured', error);
+    
+}
+
+}
