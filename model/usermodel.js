f2f100838bf81f2ca7d7413ed99491c9e53e5d6f
olivier Rukundo <rr.olivier01@gmail.com>
2025-05-20 11:59:58 +0200

    The usermodel updated with its user attributes

--- model/usermodel.js
+++ model/usermodel.js
@@ -5,5 +5,29 @@ const { DataTypes } = require('sequelize')
 const dbconn = require('../config/db');
 
 const Users = dbconn.dbconn.define('users',{
-    
+    fullName: {
+        type: DataTypes.STRING,
+        allowNull:false,
+        validate: {
+            len:[5]
+        }
+    },
+    email: {
+        type: DataTypes.STRING,
+        allowNull:false,
+        unique:true,
+        validate: {
+            isEMail:true
+        }
+
+    },
+    password: {
+        type: DataTypes.STRING,
+        allowNull:false,
+        validate:{
+            len:[8]
+        }
+    }
 })
+
+module.exports = Users;
